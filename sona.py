57220c7bc0bdb9c7a8d4fdcc999960d7937e3ae7
Akshatakhare <akshata.khare2021@vitbhopal.ac.in>
2024-06-09 02:25:45 +0530

    sona function

---
+++ sona.py
@@ -0,0 +1,16 @@
+def multipy():
+    a=5
+    b=10
+    c=a*b
+    print(c)
+calculate="Multiplying is fun!"
+
+multipy()
+print(calculate)
+
+def name():
+    x="you can do it"
+    print(x)
+
+if __name__ == "__main__":
+    name()
