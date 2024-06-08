b5cf2c983513670a4b90cf8e69540c79cc71cc1d
Akshatakhare <akshata.khare2021@vitbhopal.ac.in>
2024-06-09 02:27:25 +0530

    Create import.py

---
+++ import.py
@@ -0,0 +1,25 @@
+def multipy():
+    a=5
+    b=10
+    c=a*b
+    print(c)
+calculate="Multiplying is fun!"
+
+import math as m
+
+result=m.cos(0)
+print(result)
+
+r=m.factorial(5)/2
+print(r)
+
+# import multipy as m
+# m.multipy()
+print("starting se sb kuch print ho")
+
+from sona import multipy 
+# multipy()
+# print(calculate)
+
+from sona import name
+name()
