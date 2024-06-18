49ef78b44094f0f65c09bde74cf278d407d48d77
Akshatakhare <akshata.khare2021@vitbhopal.ac.in>
2024-06-18 23:56:59 +0530

    TEE prep

---
+++ teeprep.py
@@ -0,0 +1,39 @@
+# # import math
+# a=10
+# print(type(a),a)
+# b=2.45
+# print(type(b),b)
+# c=complex(b)
+# print(type(c),c)
+# d="hello"
+# print(type(d),d)
+# e=True
+# print(type(e),e)
+# f=[2,34,'hi']
+# print(type(f),f)
+# g=(34,'fg',90.0)
+# print(type(g),g)
+# h={45,56,78.9}
+# print(type(h),h)
+
+str1="Python"
+str2="Langauge"
+dtr3=str1+str2
+print(dtr3)
+q="Hello There"
+print((q))
+print(len(q))
+
+r="wtf am I doing"
+e=r.title()
+print(e)
+
+a = 7
+b = 4
+
+# Print bitwise AND operation
+print("a & b =", a & b)
+print("a & b =", a | b)
+print("a & b =", ~ b)
+print("a & b =", a & b)
+print("a & b =", a & b)
